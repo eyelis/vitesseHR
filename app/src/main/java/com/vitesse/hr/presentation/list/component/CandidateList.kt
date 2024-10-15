5d568e69c6a98287b6eed9df1924b077ea21d365
basil <basileye@gmail.com>
2024-10-15 13:52:42 +0200

    show no candidate

--- app/src/main/java/com/vitesse/hr/presentation/list/component/CandidateList.kt
+++ app/src/main/java/com/vitesse/hr/presentation/list/component/CandidateList.kt
@@ -2,14 +2,23 @@ package com.vitesse.hr.presentation.list.component
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.vitesse.hr.R
 import com.vitesse.hr.domain.model.Candidate
 
 
@@ -29,6 +38,30 @@ fun CandidateList(
                 onCandidateClick = onCandidateClick
             )
             Spacer(modifier = Modifier.height(16.dp))
+        }
+        if (candidates.isEmpty()) {
+            item {
+                Spacer(modifier = Modifier.height(260.dp))
+                if (candidates.isEmpty()) {
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+
+                        horizontalArrangement =  Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .align(alignment = Alignment.CenterVertically),
+                            text = stringResource(id = R.string.no_candidate)
+                        )
+                    }
+
+                }
+            }
+
         }
     }
 }
